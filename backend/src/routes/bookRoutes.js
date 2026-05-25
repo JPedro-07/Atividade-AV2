2f1cfe2eac4c71e3c18f87d075c48860cef06668
Jorge Vilela <jorge.vilela2006@gmail.com>
2026-05-25 19:14:51 -0300

    Update bookRoutes.js

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -3,7 +3,7 @@ const bookController = require('../controllers/bookController');
 
 const router = express.Router();
 
-// Rotas
+
 router.get('/search', (req, res) => bookController.searchBooks(req, res));
 router.get('/author', (req, res) => bookController.searchByAuthor(req, res));
 router.get('/:bookId', (req, res) => bookController.getBookDetails(req, res));
